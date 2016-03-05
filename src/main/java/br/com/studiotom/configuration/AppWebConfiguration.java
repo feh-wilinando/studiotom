026924e27873ddb952160c25025743384219fab2
Fernando Willian <feh.wilinando@gmail.com>
2016-03-05 16:50:02 -0300

    Added Spring webConfiguration, message resource bundle and global conversion date

---
+++ src/main/java/br/com/studiotom/configuration/AppWebConfiguration.java
@@ -0,0 +1,63 @@
+package br.com.studiotom.configuration;
+
+import br.com.studiotom.BaseClasse;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.format.datetime.DateFormatter;
+import org.springframework.format.datetime.DateFormatterRegistrar;
+import org.springframework.format.support.DefaultFormattingConversionService;
+import org.springframework.format.support.FormattingConversionService;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+/**
+ * Created by Nando on 12/02/16.
+ */
+@EnableWebMvc
+@ComponentScan(basePackageClasses = BaseClasse.class)
+public class AppWebConfiguration extends WebMvcConfigurerAdapter{
+
+    //Default View Resolver
+    @Bean
+    public InternalResourceViewResolver internalViewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+
+    //Enable static files
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+            configurer.enable();
+    }
+
+    //Global converter for Date types (Calendar, Date)
+    @Bean(name = "mvcConversionService")
+    public FormattingConversionService conversionService(){
+        DefaultFormattingConversionService service = new DefaultFormattingConversionService(true);
+
+        DateFormatterRegistrar registrar = new DateFormatterRegistrar();
+
+        registrar.setFormatter(new DateFormatter("yyy-MM-dd"));
+        registrar.registerFormatters(service);
+
+        return service;
+    }
+
+    //Messages Properties
+    @Bean(name="messageSource")
+    public MessageSource loadBundle() {
+        ReloadableResourceBundleMessageSource bundle = new ReloadableResourceBundleMessageSource();
+        bundle.setBasename("/WEB-INF/messages");
+        bundle.setDefaultEncoding("UTF-8");
+        bundle.setCacheSeconds(1);
+        return bundle;
+    }
+
+}
