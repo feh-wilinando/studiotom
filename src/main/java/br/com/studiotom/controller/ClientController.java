0524066551c98fc3d88f3174eaccdbe7b9f79b33
Fernando Willian <feh.wilinando@gmail.com>
2016-03-10 22:34:35 -0300

    MultiPart handler added

--- src/main/java/br/com/studiotom/controller/ClientController.java
+++ src/main/java/br/com/studiotom/controller/ClientController.java
@@ -1,15 +1,18 @@
 package br.com.studiotom.controller;
 
 import br.com.studiotom.dao.ClientDAO;
+import br.com.studiotom.infra.StorageFile;
 import br.com.studiotom.model.Client;
 import br.com.studiotom.model.Gender;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -25,6 +28,9 @@ public class ClientController {
 
     @Autowired
     private ClientDAO clientDAO;
+
+    @Autowired
+    private StorageFile storageFile;
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ModelAndView edit(@PathVariable Integer id){
@@ -57,11 +63,15 @@ public class ClientController {
 
     @Transactional
     @RequestMapping(method = RequestMethod.POST)
-    public ModelAndView save(@Valid Client client, BindingResult bindingResult, RedirectAttributes redirectAttributes){
+    public ModelAndView save(MultipartFile profileImg, @Valid Client client, BindingResult bindingResult, RedirectAttributes redirectAttributes){
 
         if (bindingResult.hasErrors()){
             System.out.println(client);
             return form(client);
+        }
+
+        if( ! (StringUtils.isEmpty(profileImg.getOriginalFilename())) ) {
+            client.setProfileImagePath(storageFile.write("profile-img", profileImg));
         }
 
         clientDAO.save(client);
